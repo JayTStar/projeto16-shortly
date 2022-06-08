5b2588fc6836a02be0e0643d5666cca5f9f8e511
Jorge Tadeu Soares Batalha <jsbatalha@gmail.com>
2022-06-08 12:03:42 -0300

    Rotas para usuáros

---
+++ src/router/usersRouter.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const usersRouter = Router();
+
+import { signupSchema, signinSchema } from "../schemas/schemas.js"
+import validateSchema from "../middleware/joiValidation.js"
+import { singUp } from "../controller/usersController.js"
+
+usersRouter.post("/signup", (req,res,next) => {validateSchema(req,res,next,signupSchema)}, singUp);
+usersRouter.post("/signin", (req,res,next) => {validateSchema(req,res,next,signinSchema)}, );
+usersRouter.get("/users/:id");
+usersRouter.get("/users/ranking");
+
+export default usersRouter;
