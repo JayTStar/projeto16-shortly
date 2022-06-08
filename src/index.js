8e6047d5df708430feccd358c78e7f32b315f8f5
Jorge Tadeu Soares Batalha <jsbatalha@gmail.com>
2022-06-08 12:03:49 -0300

    index

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from"dotenv";
+
+import usersRouter from "./router/usersRouter.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+app.use(usersRouter)
+
+const porta = 4000
+
+app.listen(porta, ()=> console.log(chalk.blue(`Servidor criado na porta ${porta}`)));
