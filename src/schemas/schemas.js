a77b5e38dc1fd1532262980e9838c5b34db79031
Jorge Tadeu Soares Batalha <jsbatalha@gmail.com>
2022-06-09 19:27:01 -0300

    ajeitando schemas

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -2,12 +2,12 @@ import Joi from "joi";
 
 export const signupSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
     confirmPassword: Joi.string().required()
 });
 
 export const signinSchema = Joi.object({
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required()
 });
